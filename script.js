46d35428517f67c52a5f335314214bc8cbdf0005
sagar joshi <sagarjo637@gmail.com>
2026-06-24 22:57:04 +0545

    added script.js file and target input

---
+++ script.js
@@ -0,0 +1,5 @@
+//targetting element
+let title = document.getElementById("title");
+let amount = document.getElementById("amount");
+let category = document.getElementById("category");
+let addbtn = document.getElementById("addBtn");
