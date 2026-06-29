8dab010a8c56d1e46800dbe6aea05c21c576e638
sagar joshi <sagarjo637@gmail.com>
2026-06-29 16:42:59 +0545

    Add transaction logic - get values, make object and store in array

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-//targetting element
-let title = document.getElementById("title");
-let amount = document.getElementById("amount");
-let category = document.getElementById("category");
-let addbtn = document.getElementById("addBtn");
+//storing value
+let transactions = [];
+
+let getAddBtn = document.getElementById("addBtn");
+getAddBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+//   console.log(e);
+
+  //get value
+  let getTitle = document.getElementById("title").value;
+  let getAmount = document.getElementById("amount").value;
+  let getCategory = document.getElementById("category").value;
+  let getType = document.querySelector('input[name="type"]:checked').value;
+  //   console.log(getTitle);
+  //   console.log(getAmount);
+  //   console.log(getCategory);
+  //   console.log(getType);
+
+  //make transaction object
+  let transaction = {
+    title: getTitle,
+    amount: getAmount,
+    category: getCategory,
+    type: getType,
+  };
+  transactions.push(transaction);
+  console.log(transactions);
+});
